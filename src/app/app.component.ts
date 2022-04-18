652f984925a6a43f8e705fe4f7aaedf298380695
Cao Huy Thong <thongtit1612@gmail.com>
2022-04-18 07:57:09 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'practice-app';
+  // title = 'practice-app';
+  count = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.count++;
+    }, 1000);
+  }
 }
